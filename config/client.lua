de6cee83a354a86755be5e76f34c31d3fdde83d2
P. Philipp <philippsymi@gmail.com>
2026-07-01 19:15:20 +0200

    chore: update default TruckerDepot coords

--- config/client.lua
+++ config/client.lua
@@ -1,6 +1,6 @@
 -- Importieren mit: local config = require("config.client")
 return {
-    TruckDepotCoords = vector4(-584.0, -1063.0, 22.0, 2.0),
+    TruckDepotCoords = vector4(156.8660, -3092.4927, 5.9706, 275.3402),
     DepotBlip = { sprite = 477, color = 5, scale = 0.9, name = "Truck Depot" },
     VehicleSpawnHeading = 2.0,
 }
